3973ba3aa861133991340dcf81298213b016ebe7
David Sn <divad.nnamtdeis@gmail.com>
2018-02-25 15:11:26 +0100

    Fix typo in build number

--- changelog.sh
+++ changelog.sh
@@ -10,7 +10,7 @@ GIT_COMMIT_RANGE="$DRONE_PREV_COMMIT_SHA..$DRONE_COMMIT_SHA"
 GIT_COMMIT_LOG="$(git log --format='%s (by %cn)' $GIT_COMMIT_RANGE)"
 
 # Parse log and output generated changelog to output file
-echo "<b>Changelog for build ${MAJOR_MINOR}.${TRAVIS_BUILD_NUMBER}</b>${NEWLINE}" > $PLUGIN_OUTPUT
+echo "<b>Changelog for build ${MAJOR_MINOR}-${DRONE_BUILD_NUMBER}</b>${NEWLINE}" > $PLUGIN_OUTPUT
 
 printf '%s\n' "$GIT_COMMIT_LOG" | while IFS= read -r line
 do
