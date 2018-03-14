e5fbf3f62c34f62755f225902c50f86fc546f043
David Sn <divad.nnamtdeis@gmail.com>
2018-03-14 19:14:03 +0100

    Add cache functionality

--- changelog.sh
+++ changelog.sh
@@ -8,6 +8,20 @@ if [ -z "$PLUGIN_OUTPUT" ]; then
     PLUGIN_OUTPUT="changelog.txt"
 fi
 
+if [ -z "$DRONE_CACHE" ]; then
+  DRONE_CACHE="/cache/$DRONE_REPO_OWNER/$DRONE_REPO_NAME"
+fi
+
+# Check cache for previous commit hash
+LAST_COMMIT="$DRONE_CACHE/.last_commit"
+
+if [ -f "$LAST_COMMIT" ]; then
+  DRONE_PREV_COMMIT_SHA="$(cat $LAST_COMMIT)"
+else
+  mkdir -p $DRONE_CACHE
+  echo $DRONE_COMMIT_SHA > $LAST_COMMIT
+fi
+
 # Set commit range for git log, from previous commit to latest
 GIT_COMMIT_RANGE="$DRONE_PREV_COMMIT_SHA..$DRONE_COMMIT_SHA"
 GIT_COMMIT_LOG="$(git log --format='%s (by %cn)' $GIT_COMMIT_RANGE)"
@@ -29,3 +43,6 @@ done
 # Print out changelog
 echo "Changelog for build ${MAJOR_MINOR}-${DRONE_BUILD_NUMBER}"
 cat $PLUGIN_OUTPUT
+
+# Save current commit hash to cache
+echo $DRONE_COMMIT_SHA > $LAST_COMMIT
